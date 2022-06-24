f81a824a93cd4746c8f5c4e067f545d454e50249
viiiyott <henry11n@naver.com>
2022-06-25 04:22:13 +0900

    Bug Added

--- src/kr/ac/snu/sbkim28/Main.java
+++ src/kr/ac/snu/sbkim28/Main.java
@@ -5,10 +5,9 @@ import kr.ac.snu.sbkim28.data.korean.JsonKoreanWordReader;
 import kr.ac.snu.sbkim28.data.korean.KoreanFilter;
 import kr.ac.snu.sbkim28.data.korean.KoreanWordReader;
 import kr.ac.snu.sbkim28.data.korean.KoreanWordSelector;
-import kr.ac.snu.sbkim28.legacy.analyze.WordAnalyzer;
-import kr.ac.snu.sbkim28.legacy.analyze.graph.MatrixWordGraph;
-import kr.ac.snu.sbkim28.legacy.analyze.graph.WordGraph;
-import kr.ac.snu.sbkim28.util.Timer;
+import kr.ac.snu.sbkim28.game.core.ConsoleInputPlayer;
+import kr.ac.snu.sbkim28.game.core.Environment;
+import kr.ac.snu.sbkim28.game.core.Player;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -18,12 +17,16 @@ import java.util.*;
 public class Main {
     public static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String... args) throws IOException {
-        KoreanWordReader reader = new JsonKoreanWordReader("src\\res\\WordList.json");
+        /*KoreanWordReader reader = new JsonKoreanWordReader("src\\res\\WordList.json");
         IWordSelector kws = new KoreanWordSelector(reader,
                 new KoreanFilter());
         Set<String> wordSet = kws.getWords();
         bw.write(wordSet.toString());
-        bw.flush();
-    }
+        bw.flush();*/
 
+        Player player1 = new ConsoleInputPlayer();
+        Player player2 = new ConsoleInputPlayer();
+        Environment environment = new Environment(player1, player2);
+        environment.runGame();
+    }
 }
