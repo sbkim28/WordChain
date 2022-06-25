75a89ef1c12b76f9f2276aafe1d862c9a99706db
sbkim28 <sbkim28@naver.com>
2022-06-25 13:50:50 +0900

    Add javadoc to WordAttribute andd WordClass enums.

--- src/kr/ac/snu/sbkim28/data/korean/WordAttribute.java
+++ src/kr/ac/snu/sbkim28/data/korean/WordAttribute.java
@@ -1,12 +1,23 @@
 package kr.ac.snu.sbkim28.data.korean;
 
+/**
+ * 단어 속성 enum.
+ * 표준국어대사전에 존재하는 단어 속성 중
+ * 필요한 일부만을 작성하였음.
+ * 단어 속성 검사를 편리하게 할 수 있도록 함.
+ *
+ * @author sbkim28
+ * @version 1.0
+ */
 public enum WordAttribute {
     DEFAULT("", 1),
     ARCHAISM("옛말", 2),
     DIALECT("방언", 4),
     NORTHKOREAN("북한어", 8);
 
-
+    /**
+     * 단어가 가질 수 있는 속성.
+     */
     public final String wordAttribute;
     public final int value;
 
@@ -14,7 +25,14 @@ public enum WordAttribute {
         this.wordAttribute = wordAttribute;
         this.value = value;
     }
-
+    /**
+     * String으로부터 해당하는 값을 찾아서 반환함.
+     * @param keys String. keys의 값과 일치하는
+     *            {@link WordAttribute#wordAttribute}를 가진
+     *             enum 상수를 찾음.
+     * @return 일치하는 enum 상수를 반환함. 일치하는 값이 없으면 {@link WordAttribute#DEFAULT}
+     * 값을 반환함.
+     */
     public static WordAttribute getClass(String keys){
         for (WordAttribute wc : values()){
             if(wc.wordAttribute.equals(keys))
@@ -23,19 +41,38 @@ public enum WordAttribute {
         return DEFAULT;
     }
 
-
-    public static int makePattern(WordAttribute... classes){
+    /**
+     * 속성 검사에 사용되는 pattern을 만들고 반환함.
+     * 만든 pattern은 {@link WordAttribute#matches(int, String)}에서
+     * 사용가능.
+     * @param attributes 단어의 품사. matches에서 어떤 단어의 속성이 주어진 attributes 중에 있는지
+     *                확인할 수 있음.
+     */
+    public static int makePattern(WordAttribute... attributes){
         int sum = 0;
-        for (WordAttribute clazz : classes){
+        for (WordAttribute clazz : attributes){
             sum |= clazz.value;
         }
         return sum;
     }
-
+    /**
+     * 단어의 속성이 pattern에 부합하는지 검사함.
+     * String으로 받은 단어의 품사를 enum 상수로 변환한 후,
+     * {@link WordAttribute#matches(int, WordAttribute)}를 호출하여 그 결과를 반환함.
+     * @param pattern 품사 검사에 사용되는 pattern. {@link WordAttribute#makePattern(WordAttribute...)} 참고
+     * @param key 단어의 속성.
+     * @return 단어의 속성이 pattern에 포함되었다면 true, 아니라면 false.
+     */
     public static boolean matches(int pattern, String key){
         return matches(pattern, getClass(key));
     }
-
+    /**
+     * 단어의 속성이 pattern에 부합하는지 검사함.
+     * 상수시간 내에 검사가 가능함.
+     * @param pattern 속성 검사에 사용되는 pattern. {@link WordAttribute#makePattern(WordAttribute...)} 참고
+     * @param key 단어의 속성.
+     * @return 단어의 속성이 pattern에 포함되었다면 true, 아니라면 false.
+     */
     public static boolean matches(int pattern, WordAttribute key){
         return (pattern & key.value) != 0;
     }
--- src/kr/ac/snu/sbkim28/data/korean/WordClass.java
+++ src/kr/ac/snu/sbkim28/data/korean/WordClass.java
@@ -1,5 +1,13 @@
 package kr.ac.snu.sbkim28.data.korean;
 
+/**
+ * 단어 품사 enum.
+ * 표준국어대사전에 존재하는 모든 단어 품사를 작성하였음.
+ * 단어 품사 검사를 편리하게 할 수 있도록 함.
+ *
+ * @author sbkim28
+ * @version 1.0
+ */
 public enum WordClass {
     ADJECTIVE("형용사", 1),
     AFFIX("접사", 2),
@@ -30,12 +38,24 @@ public enum WordClass {
     VERB_ADJECTIVE("동사·형용사", 67108864),
     DEFAULT(null, 0);
     public final int value;
+
+    /**
+     * 단어가 가질 수 있는 품사.
+     */
     public final String wordClass;
     WordClass(String wordClass, int value) {
         this.value = value;
         this.wordClass = wordClass;
     }
 
+    /**
+     * String으로부터 해당하는 값을 찾아서 반환함.
+     * @param keys String. keys의 값과 일치하는
+     *            {@link WordClass#wordClass}를 가진
+     *             enum 상수를 찾음.
+     * @return 일치하는 enum 상수를 반환함. 일치하는 값이 없으면 {@link WordClass#DEFAULT}
+     * 값을 반환함.
+     */
     public static WordClass getClass(String keys){
         for (WordClass wc : values()){
             if(wc.wordClass.equals(keys))
@@ -44,6 +64,13 @@ public enum WordClass {
         return DEFAULT;
     }
 
+    /**
+     * 품사 검사에 사용되는 pattern을 만들고 반환함.
+     * 만든 pattern은 {@link WordClass#matches(int, String)}에서
+     * 사용가능.
+     * @param classes 단어의 품사. matches에서 어떤 단어의 품사가 주어진 classes 중에 있는지
+     *                확인할 수 있음.
+     */
     public static int makePattern(WordClass... classes){
         int sum = 0;
         for (WordClass clazz : classes){
@@ -52,10 +79,25 @@ public enum WordClass {
         return sum;
     }
 
+    /**
+     * 단어의 품사가 pattern에 부합하는지 검사함.
+     * String으로 받은 단어의 품사를 enum 상수로 변환한 후,
+     * {@link WordClass#matches(int, WordClass)}를 호출하여 그 결과를 반환함.
+     * @param pattern 품사 검사에 사용되는 pattern. {@link WordClass#makePattern(WordClass...)} 참고
+     * @param key 단어의 품사.
+     * @return 단어의 품사가 pattern에 포함되었다면 true, 아니라면 false.
+     */
     public static boolean matches(int pattern, String key){
         return matches(pattern, getClass(key));
     }
 
+    /**
+     * 단어의 품사가 pattern에 부합하는지 검사함.
+     * 상수시간 내에 검사가 가능함.
+     * @param pattern 품사 검사에 사용되는 pattern. {@link WordClass#makePattern(WordClass...)} 참고
+     * @param key 단어의 품사.
+     * @return 단어의 품사가 pattern에 포함되었다면 true, 아니라면 false.
+     */
     public static boolean matches(int pattern, WordClass key){
         return (pattern & key.value) != 0;
     }
