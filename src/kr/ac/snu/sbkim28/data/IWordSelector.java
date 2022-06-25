5c96e51c06827497301b91775c4e5329acd3d870
sbkim28 <sbkim28@naver.com>
2022-06-25 20:28:30 +0900

    Add javadoc to Word Selector and Filter

--- src/kr/ac/snu/sbkim28/data/IWordSelector.java
+++ src/kr/ac/snu/sbkim28/data/IWordSelector.java
@@ -3,8 +3,27 @@ package kr.ac.snu.sbkim28.data;
 import java.util.Set;
 import java.util.function.Consumer;
 
+/**
+ * 단어를 선별함.
+ */
 public interface IWordSelector {
+
+    /**
+     * 모든 단어 목록을 읽고 가져옴.
+     */
     Set<String> getWords();
+
+    /**
+     * 단어 목록으로부터 순차적으로 단어를 가져옴.
+     * 단어 목록중에서 {@link Filterable#filter(IWord)}을
+     * 통과하는 단어가 나올 때까지 단어를 찾고, 해당하는 단어를 발견한다면
+     * 이를 반환함.
+     *
+     * @return 필터링 기준을 만족하는 단어를 String으로 반환함.
+     * 만약 이를 만족하는 다음의 단어가 존재하지 않을 경우
+     * null을 반환함.
+     * @see Filterable
+     */
     String filter();
 
 }
--- src/kr/ac/snu/sbkim28/data/korean/KoreanFilter.java
+++ src/kr/ac/snu/sbkim28/data/korean/KoreanFilter.java
@@ -2,14 +2,52 @@ package kr.ac.snu.sbkim28.data.korean;
 
 import kr.ac.snu.sbkim28.data.Filterable;
 
+/**
+ * 한국어 단어 필터링을 위한 클래스
+ * 한국어를 단어의 품사나 단어의 속성 등에 따라서
+ * 원하는 단어만을 필터링할 수 있는 기능을 제공.
+ * @author sbkim28
+ * @see kr.ac.snu.sbkim28.data.Filterable
+ */
 public class KoreanFilter implements Filterable<KoreanWord> {
 
+    /**
+     * WordClass에서 명사를 포함하는 품사 전체로 구성된 패턴.
+     * @see WordClass
+     * @see WordClass#makePattern(WordClass...)
+     */
     public static final int NOUN_PATTERN = 603136;
+    /**
+     * 단어가 한국어로 구성되어 있는지 검사하도록 함.
+     * 옛한글을 포함하는 단어를 제외시킴.
+     * Default true
+     */
     public boolean checkValid;
+    /**
+     * 잘못된 단어인지 검사하도록 함.
+     * 잘못된 단어일 경우 제외시킴.
+     * Default false
+     */
     public boolean checkFaulty;
+    /**
+     * 포함시킬 단어의 품사 패턴
+     * Default {@link KoreanFilter#NOUN_PATTERN}
+     * @see WordClass#makePattern(WordClass...)
+     */
     public int inClassPattern;
+    /**
+     * 단어의 최소 길이. 주어진 길이보다 짧은 단어는 제외시킴.
+     * Default 2
+     */
     public int minLength;
     // exclude
+
+    /**
+     * 제외시킬 단어의 속성 패턴.
+     * 특정 속성을 갖는 단어를 제외함.
+     * Default 0, 아무 단어도 제외시키지 않음.
+     * @see WordAttribute#makePattern(WordAttribute...)
+     */
     public int outAttributePattern;
 
     public KoreanFilter() {
@@ -37,13 +75,20 @@ public class KoreanFilter implements Filterable<KoreanWord> {
 
     public boolean isFaulty(String meaning){
         if(meaning.contains("→")){
+            // 검사를 위하여. 나중에 제거 필요.
             System.out.println(meaning);
         }else if(meaning.contains("잘못.")){
+            // 검사를 위하여. 나중에 제거 필요.
             System.out.println(meaning);
         }
         return meaning.contains("→");
     }
 
+    /**
+     * 단어가 정해진 기준을 충족시키는지 검사하여 필터링함.
+     * 해당 인스턴스의 field에서 설정된 값에 따라서 단어가
+     * 원하는 조건을 충족시키는지 검사함.
+     */
     @Override
     public boolean filter(KoreanWord word) {
         if(checkValid && !valid(word.word))
