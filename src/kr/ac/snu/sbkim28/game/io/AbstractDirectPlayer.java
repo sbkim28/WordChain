ab0083ee7834633fda71e1eca5ec07cbd2bd7f13
sbkim28 <sbkim28@naver.com>
2022-06-27 19:46:08 +0900

    Create methods and add javadoc of class AbstractDirectPlayer

--- src/kr/ac/snu/sbkim28/game/io/AbstractDirectPlayer.java
+++ src/kr/ac/snu/sbkim28/game/io/AbstractDirectPlayer.java
@@ -1,13 +1,83 @@
 package kr.ac.snu.sbkim28.game.io;
 
 import kr.ac.snu.sbkim28.game.core.AbstractPlayer;
-
+import kr.ac.snu.sbkim28.game.core.GameInitState;
+import kr.ac.snu.sbkim28.game.core.GameOverState;
+/**
+ * 직접적으로 사용자에게 입력을 받고 display를 하는 Player 클래스
+ * @author sbkim28
+ * @version 1.0.1
+ */
 public abstract class AbstractDirectPlayer extends AbstractPlayer {
 
+    /**
+     * 사용자에게 입력을 받고 처리하는 객체.
+     * {@link #createInputReceiver} method가 호출될 때 생성된다.
+     * @see PlayerInputReceiver
+     * @see #createInputReceiver()
+     */
     protected PlayerInputReceiver inputReceiver;
+
+    /**
+     * 사용자에게 게임 진행 상황을 보여주는 객체.
+     * {@link #createDisplayer()} method가 호출될 때 생성된다.
+     * @see GameDisplayer
+     * @see #createDisplayer()
+     */
     protected GameDisplayer displayer;
 
+    public AbstractDirectPlayer() {
+        super();
+    }
+
+    /**
+     * {@link #createInputReceiver()}와 {@link #createDisplayer()} methods를
+     * 호출하여 {@link #inputReceiver}와 {@link #displayer} 객체를 생성한다.
+     * 이후 게임 시작을 displayer을 통해서 알린다.
+     * @param state 게임 시작 상태
+     */
+    @Override
+    public void initialize(GameInitState state) {
+        super.initialize(state);
+        this.createDisplayer();
+        this.createInputReceiver();
+    }
+
+    /**
+     * 사용자로부터 입력받은 단어를 리턴한다.
+     * {@link #inputReceiver}를 통해서 사용자로부터 단어를 가져온다.
+     * @return {@link PlayerInputReceiver#waitUntilPlayerInput()}의 리턴값.
+     * @see PlayerInputReceiver#waitUntilPlayerInput()
+     */
+    @Override
+    public String getWord() {
+        return inputReceiver.waitUntilPlayerInput();
+    }
+
+    /**
+     * 사용자로부터 입력을 받기위해 대기하는 것을 중단한다.
+     * {@link #displayer}을 통해서 게임이 종료되었음을 알린다.
+     * @param state 게임 종료 상태
+     */
+    @Override
+    public void notifyGameOver(GameOverState state) {
+        super.notifyGameOver(state);
+        inputReceiver.haltWaiting();
+    }
+
+
+    /**
+     * {@link #inputReceiver} 객체를 생성한다.
+     * 필요에 따라서 inputReceiver 객체가
+     * null인 경우에만 새롭게 생성하도록 구현할 수 있다.
+     */
     protected abstract void createInputReceiver();
+
+    /**
+     * {@link #displayer} 객체를 생성한다.
+     * 필요에 따라서 displayer 객체가
+     * null인 경우에만 새롭게 생성하도록 구현할 수 있다.
+     */
     protected abstract void createDisplayer();
 
 }
