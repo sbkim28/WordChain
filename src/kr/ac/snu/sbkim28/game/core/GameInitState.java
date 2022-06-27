78009acaa4df7b673ff6a69faa07ee2acec21dd4
sbkim28 <sbkim28@naver.com>
2022-06-27 22:10:38 +0900

    Change State Classes

--- src/kr/ac/snu/sbkim28/game/core/GameInitState.java
+++ src/kr/ac/snu/sbkim28/game/core/GameInitState.java
@@ -1,4 +1,9 @@
 package kr.ac.snu.sbkim28.game.core;
 
 public class GameInitState {
+    public final int playerNumber;
+
+    public GameInitState(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
 }
--- src/kr/ac/snu/sbkim28/game/core/WordResultState.java
+++ src/kr/ac/snu/sbkim28/game/core/WordResultState.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class WordResultState implements Serializable {
     public final boolean success;
+    public final Cause cause;
     public final int playerNumber;
     public final String word;
 
@@ -11,5 +12,17 @@ public class WordResultState implements Serializable {
         this.success = success;
         this.playerNumber = playerNumber;
         this.word = null;
+        this.cause = Cause.SUCCESS;
+    }
+
+    public WordResultState(boolean success, Cause cause, int playerNumber, String word) {
+        this.success = success;
+        this.cause = cause;
+        this.playerNumber = playerNumber;
+        this.word = word;
+    }
+
+    public enum Cause {
+        SUCCESS, DUPLICATED, NOT_INCLUDED;
     }
 }
