e1e4a6e50b7d87c0be7351e9ab46dbe6fd82b86b
sbkim28 <sbkim28@naver.com>
2022-07-02 12:52:29 +0900

    Change GraphAnalyzer class

--- src/kr/ac/snu/sbkim28/analyze/GraphAnalyzer.java
+++ src/kr/ac/snu/sbkim28/analyze/GraphAnalyzer.java
@@ -1,12 +1,14 @@
 package kr.ac.snu.sbkim28.analyze;
 
+import kr.ac.snu.sbkim28.util.KoreanUtils;
+
 import java.util.Collection;
 import static kr.ac.snu.sbkim28.util.KoreanUtils.getSubChar;
 
 public class GraphAnalyzer {
     private int[][] matrix;
-    private boolean[] absoluteNPosition;
-    private boolean[] absolutePPosition;
+
+    private int[] charType;
     private Indexer indexer;
     private int length;
 
@@ -15,32 +17,78 @@ public class GraphAnalyzer {
         wgg.generate(wordSet);
         matrix = wgg.getMatrix();
         indexer = wgg.getIndexer();
+        length = indexer.getLength();
     }
 
-    public void createAbsoluteNPosition(){
-        absoluteNPosition = new boolean[length];
+    public int getSubIndex(int index){
+        return indexer.getIndex(
+                KoreanUtils.getSubChar(indexer.getChar(index)));
+    }
+
+    // WARNING: There is bug related to sub char...
+    public void findCharType(){
+        charType = new int[length];
+
         for (int i = 0; i<length; ++i){
-            if(!hasOutEdge(i)){
-                int sub = indexer.getIndex(getSubChar(indexer.getChar(i)));
-                if(sub != -1 && !hasOutEdge(sub)){
-                    absoluteNPosition[i] = true;
-                }
-            }
+            int sub;
+            charType[i] = !hasOutEdge(i)
+                    &&
+                    ((sub = getSubIndex(i)) == i || sub == -1 ||
+                    !hasOutEdge(sub)) ? 0 : -1;
         }
-    }
 
-    public void createAbsolutePPosition(){
-        absolutePPosition = new boolean[length];
-        for (int i = 0; i<length; ++i){
-            for (int j = 0; j<length; ++j){
-                if(matrix[i][j] > 0){
-                    if(absoluteNPosition[j]) {
-                        absolutePPosition[i] = true;
-                        break;
+        boolean isSomethingChanged;
+        do{
+            isSomethingChanged = false;
+
+            for (int i = 0; i<length; ++i){
+                int maxPriority = Integer.MIN_VALUE;
+                if(charType[i] == 0)
+                    continue;
+                int maxType = -1;
+                int sub = getSubIndex(i);
+                if(sub == -1)
+                    sub = i;
+                for (int j = 0; j<length; ++j){
+                    if(i == j) // to check self loop;
+                        continue;
+                    if(matrix[i][j] != 0 || matrix[sub][j] != 0) {
+                        int priority = getTypePriority(charType[j]);
+                        if (priority > maxPriority) {
+                            maxType = charType[j];
+                            maxPriority = priority;
+                        }
                     }
                 }
+
+                if(maxPriority > 0){
+                    int next = maxType + 1;
+                    isSomethingChanged
+                            |= next != charType[i];
+                    charType[i] = next;
+                } else if(maxPriority < 0){
+                    int next = maxType + 1 + matrix[i][i];
+                    if(sub != i)
+                        next += matrix[sub][i];
+                    isSomethingChanged
+                            |= next != charType[i];
+                    charType[i] = next;
+                }
             }
+        } while (isSomethingChanged);
+
+        int i = 0;
+    }
+
+    public static int getTypePriority(int i){
+        if(i < 0)
+            i = 0; // neutral word for 0
+        else if((i & 1) == 0){
+            i = Integer.MAX_VALUE & ~i; // n State
+        } else {
+            i |= Integer.MIN_VALUE; // p State
         }
+        return i;
     }
 
     public boolean hasOutEdge(int index){
@@ -50,5 +98,4 @@ public class GraphAnalyzer {
         }
         return false;
     }
-
 }
